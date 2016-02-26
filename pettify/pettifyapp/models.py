7a2cf7b3f0cdf379c6b0ccbb6df6c819f6d78006
CKiilu <cndeti@gmail.com>
2016-02-26 05:16:32 +0300

    One sample template

--- pettify/pettifyapp/models.py
+++ pettify/pettifyapp/models.py
@@ -6,12 +6,20 @@ from django.utils import timezone
 
 # Create your models here.
 class Animal(models.Model):
+	male = 'Male'
+	female = 'Female'
+	none = ''
+	gender_choices = (
+		('none', none),
+		('male', male),
+		('female', female),
+	)
 	image = models.FileField(null=True, blank=True)
 	category = models.CharField(max_length=100)
 	name = models.CharField(max_length=60)
 	shelter = models.CharField(max_length=60)
 	age = models.IntegerField(default=0)
-	gender = models.CharField(max_length=20)
+	gender = models.CharField(default=none, choices=gender_choices, max_length=6)
 	pub_date = models.DateTimeField('date published')
 
 	def __unicode__(self):
@@ -20,4 +28,3 @@ class Animal(models.Model):
 	def was_published_recently(self):
 		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
-	
--- pettify/pettifyapp/views.py
+++ pettify/pettifyapp/views.py
@@ -1,17 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.http import HttpResponse
 from .models import Animal
 
 # Create your views here.
 def index(request):
-    return HttpResponse("You're looking at question {}.")
+    return render(request, 'pettifyapp/index.html')
 
-def listing(request, id):
-	newest_animals = Animal.objects.order_by('pub_date')[:5]
+def listing(request):
+	newest_animals_list = Animal.objects.all()
+	# order_by('pub_date')[:5]
+	template = loader.get_template('pettifyapp/animals.html')
+	context = {
+		'newest_animals_list': newest_animals_list
+	}
+	return render(request, 'pettifyapp/animals.html', context)
 
-def category(request, id):
-	return HttpResponse("You're looking at question {}.")
+def category(request):
+	return HttpResponse("You're looking at page 3")
 
 def result(request, id):
-	return HttpResponse("You're looking at question {}.")
+	animal = get_object_or_404(Animal, pk=id)
+	return render(request, 'pettifyapp/one.html', {'animal': animal})
